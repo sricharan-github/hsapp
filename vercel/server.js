a30693247ef752a8ca9e2812044cd33f8efc63b9
Sricharan Kalavagunta <sricharan.kalavagunta5@gmail.com>
2024-11-01 18:38:01 +0530

    Server.js change

--- vercel/server.js
+++ vercel/server.js
@@ -1,65 +1,50 @@
-require('dotenv').config();
+// Require necessary packages
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const User = require('./models/User'); // User model
-const loginRoutes = require('./routes/login'); // Login routes
-const registrationRoutes = require('./routes/registration'); // Registration routes
-const userRoutes = require('./routes/user'); // User routes
-const bcrypt = require('bcrypt'); // Password hashing
-const path = require('path');
-
-// Routes
-const cors = require('cors');
+require('dotenv').config(); // Load environment variables
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS configuration
 const corsOptions = {
-  origin: process.env.REACT_APP_SERVER_URL,
+  origin: process.env.REACT_APP_SERVER_URL, // Update this if necessary
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
 
+// Use CORS middleware
 app.use(cors(corsOptions));
 
-// Middleware
-app.use(bodyParser.json()); // Parse JSON bodies
+// Middleware to parse JSON
+app.use(bodyParser.json());
 
 // MongoDB Connection
-console.log("MongoDB URI:", process.env.MONGODB_URI); 
 const dbURI = process.env.MONGODB_URI;
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB connected');
-    createDefaultUser();
   })
   .catch(err => console.error(err));
 
-  app.use('/api/users', userRoutes);
-  
-// Create default user if it doesn't exist
-const createDefaultUser = async () => {
-  const username = 'admin';
-  const password = 'Pass@123';
+// API Routes
+const loginRoutes = require('./routes/login');
+const registrationRoutes = require('./routes/registration');
+const userRoutes = require('./routes/user');
 
-  const existingUser = await User.findOne({ username });
-  if (!existingUser) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPassword });
-    await newUser.save();
-    console.log('Default user created');
-  }
-};
-
+// Handle OPTIONS method for specific routes
+app.options('/api/login', cors(corsOptions)); // Allow OPTIONS for /api/login
 app.use('/api/login', loginRoutes);
 app.use('/api/registrations', registrationRoutes);
+app.use('/api/users', userRoutes);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// The "catchall" handler: for any request that doesn't match one above, send back index.html.
+// Catchall handler for any request that doesn't match above
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
